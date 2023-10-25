8d07b1a2581644ddf5ed27da31153cc3d179de3d
Grégory Lama <gregory.lama@gmail.com>
2023-10-25 15:51:02 +0200

    refactor(filetered table): add missing column and improve modularity

--- src/js/helpers/createTable.js
+++ src/js/helpers/createTable.js
@@ -1,34 +1,50 @@
 const createTable = (datas) => {
   const table = document.createElement('table');
 
-  table.innerHTML = `
-    <thead>
-      <tr>
-        <th>Nom</th>
-        <th>Prénom</th>
-        <th>Âge</th>
-        <th>Couleur des yeux</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      ${datas
-        .map(
-          (data, index) => `
-        <tr key=${index}>
-          <td>${data.name.last}</td>
-          <td>${data.name.first}</td>
-          <td>${data.age}</td>
-          <td>${data.eyeColor}</td>
-          <td>${data.email}</td>
-        </tr>
-      `
-        )
-        .join('')}
-    </tbody>
-  `;
+  table.appendChild(createTableHeader());
+  table.appendChild(createTableBody(datas));
 
   return table;
+};
+
+const createTableHeader = () => {
+  const tableHeader = document.createElement('thead');
+
+  tableHeader.innerHTML = `
+    <tr>
+      <th>Nom</th>
+      <th>Prénom</th>
+      <th>Âge</th>
+      <th>Couleur des yeux</th>
+      <th>Email</th>
+      <th>Téléphone</th>
+      <th>Entreprise</th>
+    </tr>
+  `;
+
+  return tableHeader;
+};
+
+const createTableBody = (datas) => {
+  const tableBody = document.createElement('tbody');
+
+  tableBody.innerHTML = datas
+    .map(
+      (data, index) => `
+      <tr key=${index}>
+        <td>${data.name.last}</td>
+        <td>${data.name.first}</td>
+        <td>${data.age}</td>
+        <td>${data.eyeColor}</td>
+        <td>${data.email}</td>
+        <td>${data.phone}</td>
+        <td>${data.company}</td>
+      </tr>
+    `
+    )
+    .join('');
+
+  return tableBody;
 };
 
 export default createTable;
